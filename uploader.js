999a73875b383b24c452d304b69906044027b308
jagaa <jagaa1103@gmail.com>
2018-01-24 09:18:36 +0100

    init

---
+++ uploader.js
@@ -0,0 +1,20 @@
+// :::::::: 자르갈 :::::::::::
+
+// 이미지 올릴 url 입력 
+function setServerUrl(url){
+    console.log(url);
+}
+
+// 로컬로 저장되어 있는경우 파일 선택
+function getLocalFile(){
+
+}
+// 이미지가 blob으로 되어 있는 경우
+function getUrlFile(){
+    
+}
+
+
+function upload(image){
+    
+}
